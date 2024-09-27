797665293696327b0121420cdc4f7dc467331e36
VIGNES_Ibrahim <168725336+Ibrahimpalisse@users.noreply.github.com>
2024-09-27 16:24:23 +0200

    j'ai fini l'horloge

---
+++ Horloge/script/recupe_hrs.js
@@ -0,0 +1,21 @@
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+
+function updateClock() {
+    let now = new Date();
+    
+    hours.textContent = now.getHours().toString().padStart(2, '0');
+    minutes.textContent = now.getMinutes().toString().padStart(2, '0');
+    seconds.textContent = now.getSeconds().toString().padStart(2, '0');
+}
+
+
+setInterval(updateClock, 1000);
+
+
+updateClock();
+
+
+
+
